4fa9af74655568577a6ec0a7d2c528f19487c80e
cebix <>
2000-11-13 16:59:42 +0000

    added sources for "sheep" driver and "sheep_net" network add-on

--- BasiliskII/src/BeOS/SheepNet/sheep_net.h
+++ BasiliskII/src/BeOS/SheepNet/sheep_net.h
@@ -1,8 +1,22 @@
 /*
- *  sheep_net.h - SheepShaver net server add-on
+ *  sheep_net.h - Net server add-on for SheepShaver and Basilisk II
  *
  *  SheepShaver (C) 1997-2000 Mar"c Hellwig and Christian Bauer
- *  All rights reserved.
+ *  Basilisk II (C) 1997-2000 Christian Bauer
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
 #ifndef SHEEP_NET_H
