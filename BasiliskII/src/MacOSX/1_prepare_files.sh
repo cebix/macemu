74b3b4b669529f8512bd4450563d77380cc7c1f6
nigel <>
2003-08-02 08:19:48 +0000

    Updated Unix config.{guess,sub} which know about Darwin means that we can use them instead of looking for the OS X installed versions.

--- BasiliskII/src/MacOSX/1_prepare_files.sh
+++ BasiliskII/src/MacOSX/1_prepare_files.sh
@@ -4,31 +4,14 @@
 #
 # Run this to generate all the initial makefiles, etc.
 
-#
-# Note that we actually don't need a config.guess
-# We could instead do something like:
-#	./configure --build=powerpc-apple-darwin6.1
-#
-if [ -e /usr/libexec/config.guess ]
-then
-	#
-	# MacOS X 10.1
-	#
-	ln -sf /usr/libexec/config.guess .
-	ln -sf /usr/libexec/config.sub   .
-else
-	#
-	# MacOS X 10.2 (and later?)
-	#
-	ln -sf /usr/share/libtool/config.guess .
-	ln -sf /usr/share/libtool/config.sub .
-fi
-
+ln -sf ../Unix/config.guess .
+ln -sf ../Unix/config.sub   .
 ln -sf ../Unix/sys_unix.cpp .
 ln -sf ../Unix/timer_unix.cpp .
 ln -sf ../Unix/user_strings_unix.h .
 ln -sf ../Unix/install-sh .
-cp -pr ../../README README.txt
+cp -pr ../../INSTALL INSTALL.txt
+cp -pr ../../README  README.txt
 
 #
 # This is how I generated the button images:
