0d206db2e721045f89ee0902cee13eea411974cb
gbeauche <>
2004-06-30 08:45:14 +0000

    Use autoconf 2.5 so that we could get m4sugar macros (AS_VAR_PUSHDEF et al.) and more importantly correct macros for AC_SYS_LARGEFILE support.
    BTW, also fix test for <linux/if.h>

--- SheepShaver/src/Unix/autogen.sh
+++ SheepShaver/src/Unix/autogen.sh
@@ -5,7 +5,13 @@
 
 DIE=0
 
-PROG=SheepShaver
+PROG="SheepShaver"
+
+# Check how echo works in this /bin/sh
+case `echo -n` in
+-n) _echo_n=   _echo_c='\c';;
+*)  _echo_n=-n _echo_c=;;
+esac
 
 (autoconf --version) < /dev/null > /dev/null 2>&1 || {
         echo
@@ -15,23 +21,31 @@ PROG=SheepShaver
         DIE=1
 }
 
+(aclocal --version) < /dev/null > /dev/null 2>&1 || {
+	echo
+	echo "**Error**: Missing aclocal. The version of automake"
+	echo "installed doesn't appear recent enough."
+	echo "Get ftp://ftp.gnu.org/pub/gnu/automake-1.3.tar.gz"
+	echo "(or a newer version if it is available)"
+	DIE=1
+}
+
 if test "$DIE" -eq 1; then
         exit 1
 fi
 
-if test -z "$*"; then
-        echo "I am going to run ./configure with no arguments - if you wish "
-        echo "to pass any to it, please specify them on the $0 command line."
-fi
+aclocalinclude="$ACLOCAL_FLAGS"; \
+(echo $_echo_n " + Running aclocal: $_echo_c"; \
+    aclocal $aclocalinclude; \
+ echo "done.") && \
+(echo $_echo_n " + Running autoheader: $_echo_c"; \
+    autoheader; \
+ echo "done.") && \
+(echo $_echo_n " + Running autoconf: $_echo_c"; \
+    autoconf; \
+ echo "done.") 
 
-for dir in .
-do 
-  echo processing $dir
-  (cd $dir; \
-  aclocalinclude="$ACLOCAL_FLAGS"; \
-  aclocal $aclocalinclude; \
-  autoheader; autoconf)
-done
+rm -f config.cache
 
 if [ x"$NO_CONFIGURE" = "x" ]; then
     echo " + Running 'configure $@':"
