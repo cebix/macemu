87c8860b92c1f2e681481bc555f65b24467a97b5
gbeauche <>
2005-03-17 00:22:37 +0000

    add missing windows specific header files

---
+++ BasiliskII/src/Windows/ether_windows.h
@@ -0,0 +1,6 @@
+#ifndef _ETHER_WINDOWS_H_
+#define _ETHER_WINDOWS_H_
+
+void enqueue_packet( uint8 *buf, int sz );
+
+#endif // _ETHER_WINDOWS_H_
---
+++ BasiliskII/src/Windows/user_strings_windows.h
@@ -0,0 +1,36 @@
+/*
+ *  user_strings_unix.h - Unix-specific localizable strings
+ *
+ *  Basilisk II (C) 1997-2004 Christian Bauer
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#ifndef USER_STRINGS_UNIX_H
+#define USER_STRINGS_UNIX_H
+
+enum {
+	STR_NO_XVISUAL_ERR = 10000,
+	STR_VOSF_INIT_ERR,
+	STR_SIG_INSTALL_ERR,
+	STR_TICK_THREAD_ERR,
+	STR_NO_AUDIO_WARN,
+	STR_KEYCODE_FILE_WARN,
+	STR_KEYCODE_VENDOR_WARN,
+	STR_WINDOW_TITLE_GRABBED,
+	STR_NO_WIN32_NT_4,
+};
+
+#endif
