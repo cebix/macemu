2990a3ddfc30c0beebb46068bcabb2e211b0348e
gbeauche <>
2005-03-18 00:01:37 +0000

    add missing AboutWindow file for Windows, that have yet to be fully implemented however

---
+++ SheepShaver/src/Windows/about_window_windows.cpp
@@ -0,0 +1,30 @@
+/*
+ *  about_window_windows.cpp - "About" window, Windows implementation
+ *
+ *  SheepShaver (C) 1997-2005 Christian Bauer and Marc Hellwig
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include "about_window.h"
+
+
+/*
+ *  Open "About" window
+ */
+
+void OpenAboutWindow(void)
+{
+}
