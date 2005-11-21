9c06a00dd28451c2a601a499ac2c4d9214549305
gbeauche <>
2005-11-21 21:39:08 +0000

    GUI cosmetics from the Windows port: add "browse" button to ROM & keycodes file selectors, improve "Memory/Misc" pane to be more attractive and simplify RAM size selection, rename to "Keyboard/Mouse" pane to match the actual order of elements in the pane, make "<control>-S" save/start the config.

--- BasiliskII/src/Unix/user_strings_unix.cpp
+++ BasiliskII/src/Unix/user_strings_unix.cpp
@@ -71,7 +71,9 @@ user_string_def platform_strings[] = {
 	{STR_DSPDEVICE_FILE_CTRL, "Audio Output Device"},
 	{STR_MIXERDEVICE_FILE_CTRL, "Audio Mixer Device"},
 
-	{STR_INPUT_PANE_TITLE, "Mouse/Keyboard"},
+	{STR_BROWSE_TITLE, "Browse file"},
+	{STR_BROWSE_CTRL, "Browse..."},
+	{STR_INPUT_PANE_TITLE, "Keyboard/Mouse"},
 	{STR_KEYCODES_CTRL, "Use Raw Keycodes"},
 	{STR_KEYCODE_FILE_CTRL, "Keycode Translation File"},
 	{STR_MOUSEWHEELMODE_CTRL, "Mouse Wheel Function"},
