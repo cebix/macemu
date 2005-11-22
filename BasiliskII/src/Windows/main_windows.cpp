5fbaa95fbc43ae015f5fa682494da217170ab153
gbeauche <>
2005-11-22 06:26:35 +0000

    Windows HACK: default to the DIB driver as it looks smoother to me and doesn't require locking the surfaces, which probably the cause of apparent slowness when using the DirectX backend. This needs to be investigated more

--- BasiliskII/src/Windows/main_windows.cpp
+++ BasiliskII/src/Windows/main_windows.cpp
@@ -305,6 +305,10 @@ int main(int argc, char **argv)
 	// Load win32 libraries
 	KernelInit();
 
+	// FIXME: default to DIB driver
+	if (getenv("SDL_VIDEODRIVER") == NULL)
+	    putenv("SDL_VIDEODRIVER=windib");
+
 	// Initialize SDL system
 	int sdl_flags = 0;
 #ifdef USE_SDL_VIDEO
