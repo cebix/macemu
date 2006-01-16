998e1f7f7be8569b07f692184d5b1460c47e2513
nigel <>
2006-01-16 23:10:33 +0000

    A few more clashes with OS X headers (10.3 only?), update year

--- BasiliskII/src/MacOSX/audio_defs_macosx.h
+++ BasiliskII/src/MacOSX/audio_defs_macosx.h
@@ -6,7 +6,7 @@
  *
  *  audio_defs.h - Definitions for MacOS audio components
  *
- *  Basilisk II (C) 1997-2005 Christian Bauer
+ *  Basilisk II (C) 1997-2006 Christian Bauer
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -27,45 +27,6 @@
 #define AUDIO_DEFS_H
 
 #include "macos_util_macosx.h"
-
-
-// Sound component dispatch selector codes
-enum {
-	kSoundComponentInitOutputDeviceSelect = 1,
-	kSoundComponentSetSourceSelect = 2,
-	kSoundComponentGetSourceSelect = 3,
-	kSoundComponentGetSourceDataSelect = 4,
-	kSoundComponentSetOutputSelect = 5,
-	kDelegatedSoundComponentSelectors = 0x0100,
-	kSoundComponentAddSourceSelect = kDelegatedSoundComponentSelectors + 1,
-	kSoundComponentRemoveSourceSelect = kDelegatedSoundComponentSelectors + 2,
-	kSoundComponentGetInfoSelect = kDelegatedSoundComponentSelectors + 3,
-	kSoundComponentSetInfoSelect = kDelegatedSoundComponentSelectors + 4,
-	kSoundComponentStartSourceSelect = kDelegatedSoundComponentSelectors + 5,
-	kSoundComponentStopSourceSelect = kDelegatedSoundComponentSelectors + 6,
-	kSoundComponentPauseSourceSelect = kDelegatedSoundComponentSelectors + 7,
-	kSoundComponentPlaySourceBufferSelect = kDelegatedSoundComponentSelectors + 8
-};
-
-// Sound information selectors
-const uint32 siNumberChannels		= FOURCC('c','h','a','n');	// current number of channels
-const uint32 siChannelAvailable		= FOURCC('c','h','a','v');	// number of channels available
-const uint32 siSampleRate			= FOURCC('s','r','a','t');	// current sample rate
-const uint32 siSampleRateAvailable	= FOURCC('s','r','a','v');	// sample rates available
-const uint32 siSampleSize			= FOURCC('s','s','i','z');	// current sample size
-const uint32 siSampleSizeAvailable	= FOURCC('s','s','a','v');	// sample sizes available
-const uint32 siHardwareMute			= FOURCC('h','m','u','t');	// mute state of all hardware
-const uint32 siHardwareVolume		= FOURCC('h','v','o','l');	// volume level of all hardware
-const uint32 siHardwareVolumeSteps	= FOURCC('h','s','t','p');	// number of volume steps for hardware
-const uint32 siHardwareBusy			= FOURCC('h','w','b','s');	// sound hardware is in use
-const uint32 siHeadphoneMute		= FOURCC('p','m','u','t');	// mute state of headphone
-const uint32 siHeadphoneVolume		= FOURCC('p','v','o','l');	// volume level of headphone
-const uint32 siHeadphoneVolumeSteps	= FOURCC('h','d','s','t');	// number of volume steps for headphone
-const uint32 siSpeakerMute			= FOURCC('s','m','u','t');	// mute state of all built-in speakers
-const uint32 siSpeakerVolume		= FOURCC('s','v','o','l');	// volume level of built-in speaker
-const uint32 siDeviceName			= FOURCC('n','a','m','e');
-const uint32 siDeviceIcon			= FOURCC('i','c','o','n');
-const uint32 siHardwareFormat		= FOURCC('h','w','f','m');
 
 enum {	// ComponentResource struct
 	componentType = 0,
@@ -91,23 +52,6 @@ enum {	// ComponentResource struct
 	componentPFPlatform = 68
 };
 
-// Component feature flags
-enum {
-	k8BitRawIn				= (1 << 0),
-	k8BitTwosIn				= (1 << 1),
-	k16BitIn				= (1 << 2),
-	kStereoIn				= (1 << 3),
-	k8BitRawOut				= (1 << 8),
-	k8BitTwosOut			= (1 << 9),
-	k16BitOut				= (1 << 10),
-	kStereoOut				= (1 << 11),
-	kReverse				= (1L << 16),
-	kRateConvert			= (1L << 17),
-	kCreateSoundSource		= (1L << 18),
-	kHighQuality			= (1L << 22),
-	kNonRealTime			= (1L << 23),
-//	cmpWantsRegisterMessage	= (1L << 31)
-};
 
 enum {	// ComponentParameters struct
 	cp_flags = 0,		// call modifiers: sync/async, deferred, immed, etc
