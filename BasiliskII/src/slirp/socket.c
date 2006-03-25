61fb95dd6f1451595a977cae2b4624a4f35917b9
gbeauche <>
2006-03-25 07:57:38 +0000

    Merge in slirp updates from QEMU: - improved performance (still not visible in B2 part, especially passive mode) - set SO_REUSEADDR before calling bind() - win32 compile fix

--- BasiliskII/src/slirp/socket.c
+++ BasiliskII/src/slirp/socket.c
@@ -575,6 +575,7 @@ solisten(port, laddr, lport, flags)
 	addr.sin_port = port;
 	
 	if (((s = socket(AF_INET,SOCK_STREAM,0)) < 0) ||
+	    (setsockopt(s,SOL_SOCKET,SO_REUSEADDR,(char *)&opt,sizeof(int)) < 0) ||
 	    (bind(s,(struct sockaddr *)&addr, sizeof(addr)) < 0) ||
 	    (listen(s,1) < 0)) {
 		int tmperrno = errno; /* Don't clobber the real reason we failed */
@@ -589,7 +590,6 @@ solisten(port, laddr, lport, flags)
 #endif
 		return NULL;
 	}
-	setsockopt(s,SOL_SOCKET,SO_REUSEADDR,(char *)&opt,sizeof(int));
 	setsockopt(s,SOL_SOCKET,SO_OOBINLINE,(char *)&opt,sizeof(int));
 	
 	getsockname(s,(struct sockaddr *)&addr,&addrlen);
