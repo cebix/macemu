5ca84603bcba73c3537b4aaa13767f5e32d10ce9
asvitkine <>
2010-01-02 22:08:51 +0000

    [ Tim Douglas <timdoug@gmail.com> ] use an auto release pool around "Preferences..." menu item creation on Mac OS X

--- SheepShaver/src/MacOSX/prefs_macosx.mm
+++ SheepShaver/src/MacOSX/prefs_macosx.mm
@@ -98,8 +98,11 @@
 
 void prefs_init(void)
 {
+	NSAutoreleasePool *pool;
 	NSMenu *appMenu;
 	NSMenuItem *menuItem;
+
+	pool = [[NSAutoreleasePool alloc] init];
 
 	appMenu = [[[NSApp mainMenu] itemAtIndex:0] submenu];
 	menuItem = [[NSMenuItem alloc] initWithTitle:@"Preferences..." action:@selector(openPreferences:) keyEquivalent:@","];
@@ -108,6 +111,8 @@ void prefs_init(void)
 	[menuItem release];
 	
 	[NSApp setDelegate:[[SheepShaverMain alloc] init]];
+
+	[pool release];
 }
 
 
