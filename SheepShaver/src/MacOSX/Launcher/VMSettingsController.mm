0973ee0e16751180426e7c083c41bcd8e2476715
asvitkine <>
2010-07-27 02:57:10 +0000

    Fix compile on Snow Leopard.

--- SheepShaver/src/MacOSX/Launcher/VMSettingsController.mm
+++ SheepShaver/src/MacOSX/Launcher/VMSettingsController.mm
@@ -18,10 +18,13 @@
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-#import "VMSettingsController.h"
-
 #import "sysdeps.h"
 #import "prefs.h"
+
+// The _UINT64 define is needed to guard against a typedef mismatch with Snow Leopard headers.
+#define _UINT64
+
+#import "VMSettingsController.h"
 
 const int CDROMRefNum = -62;			// RefNum of driver
 
