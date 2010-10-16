cbedea23d2b71e5e81fffa7c410e48108f923c67
asvitkine <>
2010-10-16 04:11:18 +0000

    [C.W. Betts] This attached patch allows you to compile the Carbon Pasteboard services on Snow Leopard if you are building for 32-bit, but not if you are building for 64.
    To maintain backwards compatibility, the Carbon UI APIs aren't going to be
    stripped from the 32-bit any time soon.  However, there is no worry about
    that in 64, so they didn't include it.

--- BasiliskII/src/MacOSX/clip_macosx.cpp
+++ BasiliskII/src/MacOSX/clip_macosx.cpp
@@ -98,9 +98,9 @@ void ClipExit(void)
 
 void GetScrap(void **handle, uint32 type, int32 offset)
 {
-#if defined(MAC_OS_X_VERSION_10_6)
+#if defined(__LP64__)
 	D(bug("GetScrap handle %p, type %08x, offset %d\n", handle, type, offset));
-	#warning Carbon scrapbook function have been removed from Mac OS X 10.6+
+	#warning Carbon scrapbook function are not implemented in 64-bit mode
 #else
 	D(bug("GetScrap handle %p, type %08x, offset %d\n", handle, type, offset));
 	ScrapRef theScrap;
@@ -166,8 +166,8 @@ void GetScrap(void **handle, uint32 type, int32 offset)
 
 void PutScrap(uint32 type, void *scrap, int32 length)
 {
-#if defined(MAC_OS_X_VERSION_10_6)
-#warning Carbon scrapbook function have been removed from Mac OS X 10.6+
+#if defined(__LP64__)
+	#warning Carbon scrapbook function are not implemented in 64-bit mode
 	D(bug("PutScrap type %4.4s, data %08lx, length %ld\n", &type, scrap, length));
 #else
 	static bool clear = true;
