57416ef7b4f22e8e83a19263e682caa919fa2e77
asvitkine <>
2011-12-28 22:38:03 +0000

    add pre-generated config files for Xcode project so far, only the x86-64 config

---
+++ SheepShaver/src/MacOSX/config/config.h
@@ -0,0 +1,9 @@
+#if defined(__x86_64__)
+	#include "config-macosx-x86_64.h"
+#elif defined(__i386__)
+	#include "config-macosx-x86_32.h"
+#elif defined(__ppc__)
+	#include "config-macosx-ppc_64.h"
+#else
+	#error Unknown platform
+#endif
