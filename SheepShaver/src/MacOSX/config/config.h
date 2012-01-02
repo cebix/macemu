3c693c0b8d61adf49db11a1e2a4897abcb369980
asvitkine <>
2012-01-02 00:03:37 +0000

    add ppc build to xcode project

--- SheepShaver/src/MacOSX/config/config.h
+++ SheepShaver/src/MacOSX/config/config.h
@@ -3,7 +3,7 @@
 #elif defined(__i386__)
 	#include "config-macosx-x86_32.h"
 #elif defined(__ppc__)
-	#include "config-macosx-ppc_64.h"
+	#include "config-macosx-ppc_32.h"
 #else
 	#error Unknown platform
 #endif
