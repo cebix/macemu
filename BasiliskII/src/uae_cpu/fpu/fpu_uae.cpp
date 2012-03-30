5430e5495f5cc00701e30577cdc6108b4695e69d
asvitkine <>
2012-03-30 01:45:08 +0000

    Add correct GPUv2 attribution to fpu_ieee.cpp and fpu_uae.cpp files, to match the other files under uae_cpu/fpu, which have the same history according to CVS.

--- BasiliskII/src/uae_cpu/fpu/fpu_uae.cpp
+++ BasiliskII/src/uae_cpu/fpu/fpu_uae.cpp
@@ -1,11 +1,31 @@
 /*
- * UAE - The Un*x Amiga Emulator
+ *  fpu/fpu_uae.cpp
  *
- * MC68881 emulation
+ *  Basilisk II (C) 1997-2008 Christian Bauer
  *
- * Copyright 1996 Herman ten Brugge
+ *  MC68881/68040 fpu emulation
  *
+ *  Original UAE FPU, copyright 1996 Herman ten Brugge
+ *  Rewrite for x86, copyright 1999-2000 Lauri Pesonen
+ *  New framework, copyright 2000 Gwenole Beauchesne
+ *  Adapted for JIT compilation (c) Bernd Meyer, 2000
+ *  
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
  *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+/*
  * Following fixes by Lauri Pesonen, July 1999:
  *
  * FMOVEM list handling:
