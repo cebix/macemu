cebix/macemu

commit 0daa18ab2a7e0c8f3d813cd2e916555a0a3b623a

slirp compiles cleanly in MSVC. Use standard C prototypes instead of K&R.

BasiliskII/src/slirp/tcp_output.c
@@ -63,12 +63,10 @@ u_char  tcp_outflags[TCP_NSTATES] = {
 /*
  * Tcp output routine: figure out what should be sent and send it.
  */
-int
-tcp_output(tp)
-	register struct tcpcb *tp;
+int tcp_output(register struct tcpcb *tp)
 {
 	register struct socket *so = tp->t_socket;
-	register long len, win;
+	register u_long len, win;
 	int off, flags, error;
 	register struct mbuf *m;
 	register struct tcpiphdr *ti;
@@ -126,7 +124,7 @@ again:
 			 * to send then the probe will be the FIN
 			 * itself.
 			 */
-			if (off < so->so_snd.sb_cc)
+			if (off < (int)so->so_snd.sb_cc)
 				flags &= ~TH_FIN;
 			win = 1;
 		} else {
@@ -201,12 +199,12 @@ again:
 		 * taking into account that we are limited by
 		 * TCP_MAXWIN << tp->rcv_scale.
 		 */
-		long adv = min(win, (long)TCP_MAXWIN << tp->rcv_scale) -
+		u_int adv = min(win, (u_int)TCP_MAXWIN << tp->rcv_scale) -
 			(tp->rcv_adv - tp->rcv_nxt);
 
-		if (adv >= (long) (2 * tp->t_maxseg))
+		if (adv >= (u_int)(2 * tp->t_maxseg))
 			goto send;
-		if (2 * adv >= (long) so->so_rcv.sb_datalen)
+		if (2 * adv >= so->so_rcv.sb_datalen)
 			goto send;
 	}
 
@@ -359,7 +357,7 @@ send:
 		 */
 /*		if (len <= MHLEN - hdrlen - max_linkhdr) { */
 
-			sbcopy(&so->so_snd, off, (int) len, mtod(m, caddr_t) + hdrlen);
+			sbcopy(&so->so_snd, off, len, mtod(m, caddr_t) + hdrlen);
 			m->m_len += len;
 
 /*		} else {
@@ -435,12 +433,12 @@ send:
 	 * Calculate receive window.  Don't shrink window,
 	 * but avoid silly window syndrome.
 	 */
-	if (win < (long)(so->so_rcv.sb_datalen / 4) && win < (long)tp->t_maxseg)
+	if (win < (so->so_rcv.sb_datalen / 4) && win < tp->t_maxseg)
 		win = 0;
-	if (win > (long)TCP_MAXWIN << tp->rcv_scale)
-		win = (long)TCP_MAXWIN << tp->rcv_scale;
-	if (win < (long)(tp->rcv_adv - tp->rcv_nxt))
-		win = (long)(tp->rcv_adv - tp->rcv_nxt);
+	if (win > (u_long) (TCP_MAXWIN << tp->rcv_scale))
+		win = (u_long) (TCP_MAXWIN << tp->rcv_scale);
+	if (win < (tp->rcv_adv - tp->rcv_nxt))
+		win = (tp->rcv_adv - tp->rcv_nxt);
 	ti->ti_win = htons((u_int16_t) (win>>tp->rcv_scale));
 	
 	if (SEQ_GT(tp->snd_up, tp->snd_una)) {
@@ -530,7 +528,7 @@ send:
 	
     {
 	    
-	((struct ip *)ti)->ip_len = m->m_len;
+	((struct ip *)ti)->ip_len = (u_int16_t) m->m_len;
 
 	((struct ip *)ti)->ip_ttl = ip_defttl;
 	((struct ip *)ti)->ip_tos = so->so_iptos;
@@ -581,9 +579,7 @@ out:
 	return (0);
 }
 
-void
-tcp_setpersist(tp)
-	register struct tcpcb *tp;
+void tcp_setpersist(register struct tcpcb *tp)
 {
     int t = ((tp->t_srtt >> 2) + tp->t_rttvar) >> 1;
 

BasiliskII/src/slirp/bootp.h
@@ -115,7 +115,7 @@ struct bootp_t {
 } PACKED__;
 
 #ifdef PRAGMA_PACK_SUPPORTED
-#pragma pack(0)
+#pragma pack(PACK_RESET)
 #endif
 
 void bootp_input(struct mbuf *m);

BasiliskII/src/slirp/debug.c
@@ -16,8 +16,6 @@ int dostats = 0;
 #endif
 int slirp_debug = 0;
 
-extern char *strerror _P((int));
-
 /* Carry over one item from main.c so that the tty's restored. 
  * Only done when the tty being used is /dev/tty --RedWolf */
 extern struct termios slirp_tty_settings;
@@ -294,6 +292,7 @@ mbufstats()
 void
 sockstats()
 {
+	char addr[INET_ADDRSTRLEN];
 	char buff[256];
 	int n;
 	struct socket *so;
@@ -311,9 +310,11 @@ sockstats()
 		buff[17] = 0;
 		lprint("%s %3d   %15s %5d ",
 				buff, so->s,
-				inet_ntoa(so->so_laddr), ntohs(so->so_lport));
+				inet_ntop(AF_INET, &so->so_laddr, addr, sizeof(addr)),
+				ntohs(so->so_lport));
 		lprint("%15s %5d %5d %5d\r\n",
-				inet_ntoa(so->so_faddr), ntohs(so->so_fport),
+				inet_ntop(AF_INET, &so->so_faddr, addr, sizeof(addr)),
+				ntohs(so->so_fport),
 				so->so_rcv.sb_cc, so->so_snd.sb_cc);
 	}
 		   
@@ -325,9 +326,11 @@ sockstats()
 		buff[17] = 0;
 		lprint("%s %3d  %15s %5d  ",
 				buff, so->s,
-				inet_ntoa(so->so_laddr), ntohs(so->so_lport));
+				inet_ntop(AF_INET, &so->so_laddr, addr, sizeof(addr)),
+				ntohs(so->so_lport));
 		lprint("%15s %5d %5d %5d\r\n",
-				inet_ntoa(so->so_faddr), ntohs(so->so_fport),
+				inet_ntop(AF_INET, &so->so_faddr, addr, sizeof(addr)),
+				ntohs(so->so_fport),
 				so->so_rcv.sb_cc, so->so_snd.sb_cc);
 	}
 }

BasiliskII/src/slirp/debug.h
@@ -36,15 +36,15 @@ extern int slirp_debug;
 
 #endif
 
-void debug_init _P((char *, int));
-//void ttystats _P((struct ttys *));
-void allttystats _P((void));
-void ipstats _P((void));
-void vjstats _P((void));
-void tcpstats _P((void));
-void udpstats _P((void));
-void icmpstats _P((void));
-void mbufstats _P((void));
-void sockstats _P((void));
-void slirp_exit _P((int));
+void debug_init(char *, int);
+//void ttystats(struct ttys *);
+void allttystats(void);
+void ipstats(void);
+void vjstats(void);
+void tcpstats(void);
+void udpstats(void);
+void icmpstats(void);
+void mbufstats(void);
+void sockstats(void);
+void slirp_exit(int);
 

BasiliskII/src/slirp/if.c
@@ -7,11 +7,11 @@
 
 #include <slirp.h>
 
-int if_mtu, if_mru;
+size_t if_mtu, if_mru;
 int if_comp;
 int if_maxlinkhdr;
-int     if_queued = 0;                  /* Number of packets queued so far */
-int     if_thresh = 10;                 /* Number of packets queued before we start sending
+int if_queued = 0;                  /* Number of packets queued so far */
+int if_thresh = 10;                 /* Number of packets queued before we start sending
 					 * (to prevent allocing too many mbufs) */
 
 struct  mbuf if_fastq;                  /* fast queue (for interactive data) */
@@ -116,7 +116,8 @@ if_input(ttyp)
 	DEBUG_MISC((dfd, " read %d bytes\n", if_n));
 	
 	if (if_n <= 0) {
-		if (if_n == 0 || (errno != EINTR && errno != EAGAIN)) {
+		int error = WSAGetLastError();
+		if (if_n == 0 || (error != WSAEINTR && error != EAGAIN)) {
 			if (ttyp->up)
 			   link_up--;
 			tty_detached(ttyp, 0);

BasiliskII/src/slirp/if.h
@@ -15,8 +15,8 @@
 
 /* Needed for FreeBSD */
 #undef if_mtu
-extern int	if_mtu;
-extern int	if_mru;	/* MTU and MRU */
+extern size_t	if_mtu;
+extern size_t	if_mru;	/* MTU and MRU */
 extern int	if_comp;	/* Flags for compression */
 extern int	if_maxlinkhdr;
 extern int	if_queued;	/* Number of packets queued so far */

BasiliskII/src/slirp/ip.h
@@ -98,7 +98,7 @@ struct ip {
 } PACKED__;
 
 #ifdef PRAGMA_PACK_SUPPORTED
-#pragma pack(0)
+#pragma pack(PACK_RESET)
 #endif
 
 #define	IP_MAXPACKET	65535		/* maximum packet size */
@@ -168,7 +168,7 @@ struct	ip_timestamp {
 } PACKED__;
 
 #ifdef PRAGMA_PACK_SUPPORTED
-#pragma pack(0)
+#pragma pack(PACK_RESET)
 #endif
 
 /* flag bits for ipt_flg */
@@ -230,7 +230,7 @@ struct ipovly {
 } PACKED__;
 
 #ifdef PRAGMA_PACK_SUPPORTED
-#pragma pack(0)
+#pragma pack(PACK_RESET)
 #endif
 
 /*

BasiliskII/src/slirp/ip_icmp.c
@@ -223,9 +223,9 @@ icmp_error(msrc, type, code, minsize, message)
   if(!msrc) goto end_error;
   ip = mtod(msrc, struct ip *);
 #if DEBUG  
-  { char bufa[20], bufb[20];
-    strcpy(bufa, inet_ntoa(ip->ip_src));
-    strcpy(bufb, inet_ntoa(ip->ip_dst));
+  { char bufa[INET_ADDRSTRLEN], bufb[INET_ADDRSTRLEN];
+    inet_ntop(AF_INET, &ip->ip_src, bufa, sizeof(bufa));
+	inet_ntop(AF_INET, &ip->ip_dst, bufb, sizeof(bufb));
     DEBUG_MISC((dfd, " %.16s to %.16s\n", bufa, bufb));
   }
 #endif
@@ -244,7 +244,7 @@ icmp_error(msrc, type, code, minsize, message)
 
   /* make a copy */
   if(!(m=m_get())) goto end_error;               /* get mbuf */
-  { int new_m_size;
+  { u_int new_m_size;
     new_m_size=sizeof(struct ip )+ICMP_MINLEN+msrc->m_len+ICMP_MAXDATALEN;
     if(new_m_size>m->m_size) m_inc(m, new_m_size);
   }
@@ -299,7 +299,7 @@ icmp_error(msrc, type, code, minsize, message)
 
   /* fill in ip */
   ip->ip_hl = hlen >> 2;
-  ip->ip_len = m->m_len;
+  ip->ip_len = (u_int16_t)m->m_len;
   
   ip->ip_tos=((ip->ip_tos & 0x1E) | 0xC0);  /* high priority for errors */
 

commit 268611ca75337f978c3a6707d27cff6c0e8e6ac3

Fix SLIRP networking on OS X (and maybe other platforms?).
The bug was introduced by 0daa18ab2a7e0c8f3d813cd2e916555a0a3b623a,
which erroneously changed some signed types to unsigned types,
breaking logic involving negative numbers (e.g. when subtracting
sizes).

BasiliskII/src/slirp/tcp_output.c
@@ -66,7 +66,7 @@ u_char  tcp_outflags[TCP_NSTATES] = {
 int tcp_output(register struct tcpcb *tp)
 {
 	register struct socket *so = tp->t_socket;
-	register u_long len, win;
+	register long len, win;
 	int off, flags, error;
 	register struct mbuf *m;
 	register struct tcpiphdr *ti;
@@ -199,12 +199,12 @@ again:
 		 * taking into account that we are limited by
 		 * TCP_MAXWIN << tp->rcv_scale.
 		 */
-		u_int adv = min(win, (u_int)TCP_MAXWIN << tp->rcv_scale) -
+		long adv = min(win, TCP_MAXWIN << tp->rcv_scale) -
 			(tp->rcv_adv - tp->rcv_nxt);
 
-		if (adv >= (u_int)(2 * tp->t_maxseg))
+		if (adv >= (long)(2 * tp->t_maxseg))
 			goto send;
-		if (2 * adv >= so->so_rcv.sb_datalen)
+		if (2 * adv >= (long)so->so_rcv.sb_datalen)
 			goto send;
 	}
 