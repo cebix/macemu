54e03d96478e1476c509164876c32aba39371180
David Ludwig <dludwig@pobox.com>
2017-07-23 16:23:51 -0400

    bug-fix, Issue #2: prevent SDL2 from installing keyboard shortcuts (Command+Q, etc.) on OSX hosts, as they interfere with guest OS use

--- BasiliskII/src/MacOSX/utils_macosx.mm
+++ BasiliskII/src/MacOSX/utils_macosx.mm
@@ -28,3 +28,15 @@ void NSAutoReleasePool_wrap(void (*fn)(void))
 	fn();
 	[pool release];
 }
+
+void disable_SDL2_macosx_menu_bar_keyboard_shortcuts() {
+	for (NSMenuItem * menu_item in [NSApp mainMenu].itemArray) {
+		if (menu_item.hasSubmenu) {
+			for (NSMenuItem * sub_item in menu_item.submenu.itemArray) {
+				sub_item.keyEquivalent = @"";
+				sub_item.keyEquivalentModifierMask = 0;
+			}
+		}
+	}
+}
+
