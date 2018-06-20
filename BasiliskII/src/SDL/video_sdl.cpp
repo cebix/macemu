6039a60549dd7ef815115417c1ada3aa52bc50d3
kanjitalk755 <kanjitalk755@yahoo.co.jp>
2018-06-20 21:26:11 +0900

    fix merge error

--- BasiliskII/src/SDL/video_sdl.cpp
+++ BasiliskII/src/SDL/video_sdl.cpp
@@ -2235,7 +2235,7 @@ static int redraw_func(void *arg)
 
 		// Wait
 		next += VIDEO_REFRESH_DELAY;
-		uint64 delay = int32(next - GetTicks_usec());
+		int32 delay = int32(next - GetTicks_usec());
 		if (delay > 0)
 			Delay_usec(delay);
 		else if (delay < -VIDEO_REFRESH_DELAY)
