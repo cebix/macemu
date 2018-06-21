9c0886b2b835a17a99146b209d8392b3282eeefd
kanjitalk755 <kanjitalk755@yahoo.co.jp>
2018-06-21 22:55:35 +0900

    set version to 2.5

--- SheepShaver/src/MacOSX/config/config-macosx-x86_64.h
+++ SheepShaver/src/MacOSX/config/config-macosx-x86_64.h
@@ -415,7 +415,7 @@
 #define PACKAGE_NAME "SheepShaver"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "SheepShaver 2.4"
+#define PACKAGE_STRING "SheepShaver 2.5"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "SheepShaver"
@@ -424,7 +424,7 @@
 #define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "2.4"
+#define PACKAGE_VERSION "2.5"
 
 /* Define if the __PAGEZERO Mach-O Low Memory Globals hack works on this
    system. */
