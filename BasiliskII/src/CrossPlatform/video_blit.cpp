dc8bb7e2a3e3938a915f9c0f98a510637a3872df
Ricky Zhang <rickyzhang@gmail.com>
2020-06-25 16:56:40 -0400

    Back out the hack from kanjitalk755
    - In 8 bit color game, the hack crashed the app.
    - After backing out the hack, I can play 256 color game with displaycolordepth 8.
    - By default, the displaycolordepth is 0.
    
    Signed-off-by: Ricky Zhang <rickyzhang@gmail.com>

--- BasiliskII/src/CrossPlatform/video_blit.cpp
+++ BasiliskII/src/CrossPlatform/video_blit.cpp
@@ -523,21 +523,11 @@ bool Screen_blitter_init(VisualFormat const & visual_format, bool native_byte_or
 #else
 	const bool use_sdl_video = false;
 #endif
-#if REAL_ADDRESSING || DIRECT_ADDRESSING || USE_SDL_VIDEO
+#if REAL_ADDRESSING || DIRECT_ADDRESSING
 	if (mac_depth == 1 && !use_sdl_video && !visual_format.fullscreen) {
 
 		// Windowed 1-bit mode uses a 1-bit X image, so there's no need for special blitting routines
 		Screen_blit = Blit_Copy_Raw;
-
-#if __MACOSX__ && !defined(SHEEPSHAVER)
-	// dludwig@pobox.com, HACK: This works on OSX (64-bit, at least), but not Linux (32-bit?).  Why?
-	//   To note, __MACOSX__ is an SDL-declared macro (for platform identification at compile time).
-	} else if (mac_depth == 16) {
-
-		Screen_blit = Blit_Copy_Raw;
-
-#endif
-	
 	} else {
 
 		// Compute RGB shift values
