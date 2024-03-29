979523b5caf22a6d4c33090095b1eb91ebcdebe3
kanjitalk755 <kanjitalk755@yahoo.co.jp>
2024-03-29 21:11:46 +0900

    Fix #195

--- SheepShaver/src/Unix/main_unix.cpp
+++ SheepShaver/src/Unix/main_unix.cpp
@@ -697,7 +697,7 @@ static bool init_sdl()
 	assert(sdl_flags != 0);
 
 #ifdef USE_SDL_VIDEO
-#if REAL_ADDRESSING && !defined(__MACOSX__)
+#if REAL_ADDRESSING && defined(GDK_WINDOWING_WAYLAND)
 	// Needed to fix a crash when using Wayland
 	// Forces use of XWayland instead
 	setenv("SDL_VIDEODRIVER", "x11", true);
